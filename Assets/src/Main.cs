cb49025354a0df6a76a6073e54d662a73acfc75a
dumuller <eduardomuller295@gmail.com>
2017-04-15 11:14:06 -0300

    Recombinação Agentes.

--- Assets/src/Main.cs
+++ Assets/src/Main.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Utils.dp;
 using UnityEngine.SceneManagement;
+using System;
 
 public class Main : MonoBehaviour {
 	[SerializeField] public FabricaInimigo fabricaInimigo;
@@ -19,9 +20,25 @@ public class Main : MonoBehaviour {
 	}
 	
 	void Update () {
-		foreach (Inimigo inimigo in inimigos) {
+		foreach (Inimigo inimigo in inimigos) {			
+			if (inimigo.estado == Enums.EstadoEnum.Passeio){
 			Vector3 posicao = inimigo.transform.position;
-
+			float [] PosicaoX = new float[inimigos.Count];
+			float [] PosicaoZ = new float[inimigos.Count];
+			float [] PosicaoY = new float[inimigos.Count];
+				for (var i = 0; i < inimigos.Count; i++) {
+					PosicaoX [i] = posicao.x;
+					PosicaoZ [i] = posicao.z;
+					PosicaoY [i] = posicao.y;
+				}
+				for (var j = 0; j < inimigos.Count; j++) {
+					System.Random rnd = new System.Random();
+					int QualPosicao;
+					QualPosicao = rnd.Next(0, inimigos.Count);
+					Vector3 novaPosicao = new Vector3(PosicaoX [QualPosicao], PosicaoZ [j], PosicaoY [j]);
+					inimigo.transform.position = novaPosicao;
+				}
+			}
 		}
 	}
 
